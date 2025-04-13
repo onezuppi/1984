b43d37f3aea8461e0035395694eb6be14f53c00f
vtyric <misha.torno@gmail.com>
2025-04-13 10:53:55 +0500

    small refactoring

--- apps/front/src/app/pages/profile/profile.page.ts
+++ apps/front/src/app/pages/profile/profile.page.ts
@@ -4,7 +4,6 @@ import { TuiButton } from '@taiga-ui/core';
 import { CommonModule } from '@angular/common';
 import { TuiAvatar } from '@taiga-ui/kit';
 import { AuthService } from '../../services/auth.service';
-import { BACKEND_API_URL } from '../../tokens/app-config.token';
 
 
 @Component({
@@ -15,29 +14,14 @@ import { BACKEND_API_URL } from '../../tokens/app-config.token';
         CommonModule,
         TuiAvatar
     ],
-    template: `
-        <div style="max-width: 400px; margin: 50px auto; text-align: center;" *ngIf="profile$ | async as profile; else loading">
-            <tui-avatar *ngIf="profile.avatar; else noAvatar" [src]="profile.avatar" size="l"></tui-avatar>
-            <ng-template #noAvatar>
-                <tui-avatar>{{ profile.name }}</tui-avatar>
-            </ng-template>
-            <h3>{{ profile.name }}</h3>
-            <button tuiButton appearance="primary" size="m" (click)="logout()">
-                Выйти
-            </button>
-        </div>
-        <ng-template #loading>
-            Загрузка профиля...
-        </ng-template>
-    `
+    templateUrl: './profile.page.html',
+    styleUrls: ['./profile.page.scss']
 })
 export class ProfilePage implements OnInit {
 
     protected profile$!: Observable<{ name: string; avatar: string | null }>;
 
     private readonly auth: AuthService = inject(AuthService);
-
-    private readonly apiUrl: string = inject(BACKEND_API_URL);
 
     ngOnInit(): void {
         this.profile$ = this.auth.getUserData();
