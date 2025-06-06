cf6f371433e8edcd5b59e94f3ae6f5b740cb3eb3
Sergey Dorokhov <seregapax@gmail.com>
2025-06-06 21:19:20 +0500

    add loading

--- apps/front/src/app/components/channel-card/channel-card.component.ts
+++ apps/front/src/app/components/channel-card/channel-card.component.ts
@@ -1,15 +1,18 @@
 import { ChangeDetectionStrategy, Component, Input } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { Clipboard } from '@angular/cdk/clipboard';
 import { Channel } from '../../services/channels/channel.service';
 import { MatCardModule } from '@angular/material/card';
 import { MatChipsModule } from '@angular/material/chips';
 import { MatIconModule } from '@angular/material/icon';
 import { MatButtonModule } from '@angular/material/button';
+import { MatSnackBar } from '@angular/material/snack-bar';
+import { MatTooltip } from '@angular/material/tooltip';
 
 @Component({
     selector: 'app-channel-card',
     standalone: true,
-    imports: [CommonModule, MatCardModule, MatChipsModule, MatIconModule, MatButtonModule],
+    imports: [CommonModule, MatCardModule, MatChipsModule, MatIconModule, MatButtonModule, MatTooltip],
     templateUrl: './channel-card.component.html',
     styleUrls: ['./channel-card.component.scss'],
     changeDetection: ChangeDetectionStrategy.OnPush,
@@ -17,4 +20,28 @@ import { MatButtonModule } from '@angular/material/button';
 export class ChannelCardComponent {
     @Input()
     public channel!: Channel;
+
+    constructor(
+        private readonly _clipboard: Clipboard,
+        private readonly _snackBar: MatSnackBar
+    ) {}
+
+    public copyLink(event: Event, url: string): void {
+        event.stopPropagation();
+        const successful = this._clipboard.copy(url);
+
+        if (successful) {
+            this._snackBar.open('Ссылка скопирована в буфер обмена', 'ОК', {
+                duration: 2000,
+                horizontalPosition: 'center',
+                verticalPosition: 'top'
+            });
+        } else {
+            this._snackBar.open('Не удалось скопировать ссылку', 'Закрыть', {
+                duration: 2000,
+                horizontalPosition: 'center',
+                verticalPosition: 'top'
+            });
+        }
+    }
 }
